e61a7ea4d41863d71d47e352d1967ebcaf891c86
Chirag Sharma <chirag.sharma@thewasserstoff.com>
2024-03-27 13:27:22 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MEU 
+
+A Web-App hosted on ICP to connect with people and sign on Chain Aggrements.
+
+
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>MEU - Meet , Establish & Unite</h1>
+    </>
+  )
+}
+
+export default App
