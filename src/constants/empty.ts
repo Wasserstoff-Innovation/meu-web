20110a447243dce841ca44445a99506901343e4b
Chirag Sharma <chirag.sharma@thewasserstoff.com>
2024-04-02 13:22:33 +0530

    add redux

---
+++ src/constants/empty.ts
@@ -0,0 +1,18 @@
+export const EmptyUser = {
+  id: "",
+  name: "",
+  email: "",
+  mobile: "",
+  location: "",
+  username: "",
+  bio: "",
+  avatar: "",
+  pronounns: "",
+  interests: [],
+  purpose: "",
+  socials: {
+    linkedin: "",
+    twitter: "",
+    telegram: "",
+  },
+};
