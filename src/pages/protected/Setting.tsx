c9e26fbcd9ab04c9f57f17e15a61ffaf375780f2
kgwstf2024 <137784431+kgDBM1996@users.noreply.github.com>
2024-04-04 19:24:03 +0530

    settings

--- src/pages/protected/Setting.tsx
+++ src/pages/protected/Setting.tsx
@@ -7,22 +7,22 @@ const Settings = () => {
     {
       icon: "/interests.svg",
       name: "Interests",
-      path: "/interests" // Define the path for each
+      path: "/settings/interests" // Define the path for each
     },
     {
       icon: "/search.svg",
       name: "Purpose",
-      path: "/purpose"
+      path: "/settings/purpose"
     },
     {
       icon: "/privacy.svg",
       name: "Privacy",
-      path: "/privacy"
+      path: "/settings/privacy"
     },
     {
       icon: "/other.svg",
       name: "Other",
-      path: "/other"
+      path: "/settings/other"
     },
   ];
 
@@ -30,38 +30,42 @@ const Settings = () => {
     {
       icon: "/share.svg",
       name: "Share MEU",
-      path: "/share"
+      path: "/settings/share"
     },
     {
       icon: "/grade.svg",
       name: "Rate MEU",
-      path: "/rate"
+      path: "/settings/rate"
     },
     {
       icon: "/help.svg",
       name: "Help",
-      path: "/help"
+      path: "/settings/help"
     },
     {
       icon: "/about.svg",
       name: "About",
-      path: "/about"
+      path: "/settings/about"
     },
   ];
   const handleNavigation = (path:string) => {
     Navigate(path); // Navigate to the specified path
   };
+  const handlenavigate = () =>{
+    Navigate("/settings/editProfile")
+  }
 
   return (
     <div className="flex flex-col justify-between h-[95vh] gap-6">
-      <div className="flex gap-2  text-2xl">
+      <div className="flex gap-2  text-2xl cursor-pointer">
         <img src="/left-arrow.svg" alt="back-navigation w-4 h-4" />
         <div>setting</div>
       </div>
       <div className="flex flex-col justify-between h-full">
-        <div className="flex gap-3 bg-[#313437] p-5 rounded-md cursor-pointer">
+        <div className="flex gap-3 bg-[#313437] p-5 rounded-md cursor-pointer" onClick={handlenavigate}>
           <div className="w-12 h-12 rounded-full bg-white">
-            <img src="" alt="" />
+            <img 
+            src="https://s3-alpha-sig.figma.com/img/41bd/111a/617b61c4709a03d3444b62e630fc9ca2?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fzFa2JZZQFXYmu2ML7zzufdA3URFClJHqeYGy2ifR40nDr8gYWI4FktGK4hFqXsnJ6GA~J3JhYmZLcNlSJKFvzY5mV6DQXcLnFAHIAJFlz2oKeIu6hXZuaeCgCXFt8j1anZ9MtY9vsvnN5scB1LgR9S30lC-p05jU3YBJkkYurfQ787CAo7TMJD53IHpHrK-cXQ45PIIQJam5iH~Nm3MZZAjL2nxljDTiZ0D-0C0udb1sfeghjooJ~LiPL-0l91dwifpbYgpOv9ygcVaDUX3eL5KGB1o8F3WaIbc2jbjx~t5enk2byc3YLnN3jSUGDHETbewpqKjQtyex4ryK6S0QA__" alt="" className="rounded-full" />
           </div>
           <div className="flex flex-col">
             <div className="font-bold text-xl">John Doe</div>
@@ -70,7 +74,7 @@ const Settings = () => {
         </div>
 
         <div className="font-semibold text-lg mt-4">SETTING</div>
-        <div className="flex flex-col bg-[#313437] rounded-md">
+        <div className="flex flex-col bg-[#313437] rounded-md cursor-pointer">
           {Data.map((data, index) => (
               <div key={index} onClick={() => handleNavigation(data.path)}> 
                 <div className="flex gap-2 items-center p-2 ">
@@ -87,8 +91,8 @@ const Settings = () => {
         <div className="font-semibold text-lg mt-4">ABOUT</div>
         <div className="flex flex-col bg-[#313437] rounded-md">
           {About.map((data, index) => (
-            <div>
-               <div key={index} onClick={() => handleNavigation(data.path)}> 
+            <div key={index}>
+               <div  onClick={() => handleNavigation(data.path)}> 
               <div className="flex gap-2 items-center p-2 cursor-pointer">
                 <img src={data.icon} alt="icon" className="w-4 h-4" />
                 <div>{data.name}</div>
--- src/pages/protected/Setting/Share.tsx
+++ src/pages/protected/Setting/Share.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useNavigate  } from "react-router-dom";
 
-const share = () => {
+
+const Share = () => {
+  // State variables for each switch
+  const Navigate =useNavigate ();
+  const backNavigate = () =>{
+    Navigate("/settings")
+  }
+
   return (
-    <div>share</div>
-  )
-}
+    <div className="flex flex-col justify-between gap-6">
+      <div className="flex gap-2 text-2xl z-10 relative top-4 left-0 items-center ">
+        <img src="/left-arrow.svg" alt="back-navigation" className="w-4 h-4 cursor-pointer"
+        onClick={backNavigate}
+        />
+        <div>Share</div>
+      </div>
+      {/* rest code here */}
+    </div>
+  );
+};
 
-export default share
+export default Share;
