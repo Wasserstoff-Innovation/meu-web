2fb33c489e8edfb634727479faf0c95dfd486ce5
Chirag Sharma <chirag.sharma@thewasserstoff.com>
2024-04-08 16:19:48 +0530

    code for signng

--- src/components/Home/Navbar.tsx
+++ src/components/Home/Navbar.tsx
@@ -1,8 +1,38 @@
+// import { AuthClient, LocalStorage } from "@dfinity/auth-client";
+// import { Ed25519KeyIdentity } from "@dfinity/identity";
 import { Navbar, NavbarBrand } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 
 export default function App() {
   const navigate = useNavigate();
+
+  const checkSign = async () => {
+    // const authClient = await AuthClient.create({
+    //   storage: new LocalStorage(),
+    //   keyType: "Ed25519",
+    // });
+    // // await authClient.login({
+    // //   // 7 days in nanoseconds
+    // //   maxTimeToLive: BigInt(7 * 24 * 60 * 60 * 1000 * 1000 * 1000),
+    // //   identityProvider: "https://identity.internetcomputer.org",
+    // //   onSuccess: async () => {
+    // //     console.log(authClient);
+    // //   },
+    // // });
+    // const identity = authClient.getIdentity();
+    // console.log("identity", identity, authClient);
+    // const identity = Ed25519KeyIdentity.generate();
+    // console.log("identity", identity);
+    // const publicKey = identity.getPublicKey();
+    // console.log("publicKey", publicKey);
+    // const keyPair = identity.getKeyPair()
+    // console.log("keyPair", keyPair);
+    // const someData = "Hello World!";
+    // const signature = await identity.sign(Buffer.from(someData));
+    // console.log("signature", signature);
+    // const isValid = Ed25519KeyIdentity.verify( signature ,  Buffer.from(someData), publicKey.toDer());
+    // console.log("isValid", isValid);
+  };
 
   return (
     <>
@@ -17,6 +47,7 @@ export default function App() {
             src="./qr_code_scanner.svg"
             alt="qr-code"
             className="size-6 cursor-pointer"
+            onClick={checkSign}
           />
           <img
             src="./tune.svg"
