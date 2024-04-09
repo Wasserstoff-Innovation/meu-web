df4f622e4b78754b7902ecf0366d9747c8991a8c
Chirag Sharma <chirag.sharma@thewasserstoff.com>
2024-04-09 15:50:18 +0530

    google places API add

---
+++ src/components/maps/PlaceSearch.tsx
@@ -0,0 +1,33 @@
+import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import { useState } from "react";
+import { findPlaces } from "../../utils/places";
+
+const PlaceSearch = () => {
+  const [value, setValue] = useState<string | null>(null);
+  const [results, setResults] = useState<google.maps.places.Place[]>([]);
+  return (
+    <Autocomplete
+      name="location"
+      type="text"
+      placeholder="Gurgugram, Haryana, India"
+      isClearable
+      variant="bordered"
+      onChange={async (e) => {
+        const places = await findPlaces(e.target.value);
+        setResults(places);
+      }}
+      items={results}
+      className="max-w-xs"
+      selectedKey={value}
+      onSelectionChange={(e) => {
+        console.log(e);
+      }}
+    >
+      {(item) => (
+        <AutocompleteItem key={item.id}>{item.displayName}</AutocompleteItem>
+      )}
+    </Autocomplete>
+  );
+};
+
+export default PlaceSearch;
---
+++ src/utils/places.ts
@@ -0,0 +1,22 @@
+export const findPlaces = async (text: string) => {
+  const { Place } = (await google.maps.importLibrary(
+    "places"
+  )) as google.maps.PlacesLibrary;
+  const request = {
+    textQuery: text,
+    fields: ["displayName", "location", "businessStatus"],
+    language: "en-US",
+    maxResultCount: 8,
+    region: "us",
+    useStrictTypeFiltering: false,
+  };
+
+  const { places } = await Place.searchByText(request);
+
+  if (places.length) {
+    console.log(places);
+  } else {
+    console.log("No results");
+  }
+  return places;
+};
