e0a1777aa996b54fae7f68908995f637906016cc
amitpatel-wsrt <amit.patel@thewasserstoff.com>
2024-04-09 16:56:16 +0530

    npm run build successfully run

--- src/pages/protected/Setting/About.tsx
+++ src/pages/protected/Setting/About.tsx
@@ -1,26 +1,30 @@
-import React, { useState } from "react";
-import { Switch } from "@nextui-org/react";
+// import React, { useState } from "react";
+// import { Switch } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 const About = () => {
   const Navigate = useNavigate();
-  const [isVisible, setIsVisible] = useState(true);
-  const [isNotificationEnabled, setIsNotificationEnabled] = useState(false);
+  // const [isVisible, setIsVisible] = useState(true);
+  // const [isNotificationEnabled, setIsNotificationEnabled] = useState(false);
 
-  const backNavigate = () =>{
-    Navigate("/settings")
-  }
+  const backNavigate = () => {
+    Navigate("/settings");
+  };
 
   return (
     <div className="flex flex-col justify-between gap-6">
       <div className="flex gap-2 text-2xl z-10 relative top-4 left-0 items-center">
-        <img src="/left-arrow.svg" alt="back-navigation" className="w-4 h-4 cursor-pointer"
-        onClick={backNavigate}/>
+        <img
+          src="/left-arrow.svg"
+          alt="back-navigation"
+          className="w-4 h-4 cursor-pointer"
+          onClick={backNavigate}
+        />
         <div>About</div>
       </div>
       <div className="flex flex-col gap-4">
         <div className=" flex gap-3 border items-center border-gray-400 px-4 py-2 rounded-md w-full">
           <img
-            src= "/icons/contract.svg"
+            src="/icons/contract.svg"
             alt="icon"
             className="cursor-pointer w-5 h-5"
           />
@@ -40,10 +44,8 @@ const About = () => {
             alt="block"
             className="cursor-pointer w-5 h-5"
           />
-         <div>Release Notes</div>
+          <div>Release Notes</div>
         </div>
-        
-        
       </div>
     </div>
   );
--- src/pages/protected/Setting/Purpose.tsx
+++ src/pages/protected/Setting/Purpose.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+
 import { useNavigate  } from "react-router-dom";
 import { Radio, RadioGroup } from "@nextui-org/react";
 import { purposes } from "../../../constants/purpose";
