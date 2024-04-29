bdde687d064e11e647697d3948dd42b28b5e47f5
Chirag Sharma <chirag.sharma@thewasserstoff.com>
2024-04-29 13:45:59 +0530

    loading animation and location

--- src/pages/protected/Home/MapView.tsx
+++ src/pages/protected/Home/MapView.tsx
@@ -1,9 +1,13 @@
 import { useGeolocation } from "@shubhamssingh/u-hook";
-import { Map, AdvancedMarker, Pin } from "@vis.gl/react-google-maps";
+import TopBarProgress from "react-topbar-progress-indicator";
+import { Map, AdvancedMarker, useApiIsLoaded } from "@vis.gl/react-google-maps";
+import { loaderConfig } from "../../../config";
 
+TopBarProgress.config(loaderConfig);
 const MapView = () => {
   const { loading, error, data } = useGeolocation();
-  if (loading) return <p>Loading...</p>;
+  const apiIsLoaded = useApiIsLoaded();
+  if (loading || !apiIsLoaded) return <TopBarProgress />;
   if (data.latitude && data.longitude) {
     const position = { lat: data.latitude, lng: data.longitude };
     return (
@@ -11,19 +15,25 @@ const MapView = () => {
         <Map
           mapId={"bf51a910020fa25a"}
           defaultCenter={position}
-          gestureHandling={'greedy'}
-          defaultZoom={11}
-          className="flex-1  "
+          gestureHandling={"greedy"}
+          defaultZoom={14}
+          className="flex-1"
         >
           <AdvancedMarker
             position={position}
             title={"AdvancedMarker with customized pin."}
           >
-            <Pin
-              background={"#22ccff"}
-              borderColor={"#1e89a1"}
-              glyphColor={"#0f677a"}
-            ></Pin>
+            <div
+              className="bg-primary-500 text-white p-2 rounded-lg"
+              style={{
+                width: 20,
+                height: 20,
+                position: "absolute",
+                top: 0,
+                left: 0,
+                transform: "translate(-50%, -50%)",
+              }}
+            ></div>
           </AdvancedMarker>
         </Map>
       </div>
