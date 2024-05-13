abecc6d80f54f556bc43c1af05a2de8a7dd6869f
sryuvarajwstf <yuvraj.sr@thewasserstoff.com>
2024-05-13 12:55:28 +0530

    linked.ts file changes for removed unwanted changes

--- src/api/verification/linkedin.ts
+++ src/api/verification/linkedin.ts
@@ -1,6 +1,6 @@
 import { getURLWithQueryParams } from "./twitter";
 
-const LinkedIn_AUTH_URL = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&";
+const LinkedIn_AUTH_URL = "https://www.linkedin.com/oauth/v2/authorization";
 const clientId = import.meta.env.VITE_LINKEDIN_CLIENT_ID as string;
 const frontendURL = import.meta.env.VITE_FRONTEND_URL as string;
 const state = import.meta.env.VITE_CSRF_TOKEN;
@@ -9,7 +9,7 @@ export const getLinkedinOAuthUrl = () =>
   getURLWithQueryParams(LinkedIn_AUTH_URL, {
     response_type: "code",
     client_id: clientId,
-    redirect_uri: `${frontendURL}/auth/linkedin/callback`,
+    redirect_uri: `${frontendURL}/auth/callback/linkedin`,
     state: state,
     scope: "openid profile w_member_social email",
   });
